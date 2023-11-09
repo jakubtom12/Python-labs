d213c32248aed5f835be2e5727002049723342a5
jakubtom12 <jakubtom@student.agh.edu.pl>
2023-11-10 00:48:27 +0100

    korekta zad2_3

--- Lab2/zad2_3.py
+++ Lab2/zad2_3.py
@@ -8,34 +8,35 @@ def generate_random_data(length): #funkcja do tworzenia danych losowych
     return [random.randint(0, 10**6) for _ in range(length)]
 
 #Test funkcji 
-data_sizes = [10**5, 2*10**5, 10**6, 2*10**6]
-process_counts = [1, 2, 4, multiprocessing.cpu_count()]
+data_sizes = [10**5, 10**6, 2*10**6]
+process_counts = [1, 2, 3]
 
 results = {}
 
-for size in data_sizes:
-    data = generate_random_data(size)
+if __name__ == '__main__':
+    for size in data_sizes:
+        data = generate_random_data(size)
+        for processes in process_counts:
+            start_time = time.time()
+            parallel_sort(data, processes)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            results[(size, processes)] = elapsed_time
+
+    #Pokazanie rezultatu 
+    fig, ax = plt.subplots()
+
     for processes in process_counts:
-        start_time = time.time()
-        parallel_sort(data, processes)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        results[(size, processes)] = elapsed_time
+        times = [results[(size, processes)] for size in data_sizes]
+        ax.plot(data_sizes, times, label=f'{processes} processes')
 
-#Pokazanie rezultatu 
-fig, ax = plt.subplots()
+    ax.set_xlabel('Data size')
+    ax.set_ylabel('Time (seconds)')
+    ax.legend()
+    ax.grid(True)
 
-for processes in process_counts:
-    times = [results[(size, processes)] for size in data_sizes]
-    ax.plot(data_sizes, times, label=f'{processes} processes')
+    #Wnioski z wykresu
+    #Czas sortowania rosnie ze wzrostem rozmiaru danych
+    #Wiecej procesow przyspiesza sortowanie dla wiekszej ilosci danych
 
-ax.set_xlabel('Data size')
-ax.set_ylabel('Time (seconds)')
-ax.legend()
-ax.grid(True)
-
-#Wnioski z wykresu
-#Czas sortowania rosnie ze wzrostem rozmiaru danych
-#Wiecej procesow przyspiesza sortowanie dla wiekszej ilosci danych
-
-plt.show()
+    plt.show()
