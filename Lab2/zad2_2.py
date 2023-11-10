0db9bc824161708bad2ee41b4076df1aac6a44f9
jakubtom12 <117313854+jakubtom12@users.noreply.github.com>
2023-11-10 13:22:52 +0100

    Update zad2_2.py
    koretka v3

--- Lab2/zad2_2.py
+++ Lab2/zad2_2.py
@@ -1,10 +1,11 @@
 import multiprocessing
 
-"""Funtion to merge two sorted list which are sorted in the same time"""
-
 def merge_sorted_lists(list1, list2):
     result = []
     i = j = 0
+
+    list1 = try_list(list1)
+    list2 = try_list(list2)
 
     while i < len(list1) and j < len(list2):
         if list1[i] < list2[j]:
@@ -24,19 +25,30 @@ def merge_sorted_lists(list1, list2):
 
     return result
 
+
+def try_list(list1):
+    try:
+        len(list1)
+    except TypeError:
+        list1 = [list1]
+    return list1
+
+
+def check_sort(data):
+    try:
+        return sorted(data)
+    except TypeError:
+        if isinstance(data, int):
+            return data
+        else:
+            raise
+
 def parallel_sort(data, processes):
-    """Function for parallel sorting two data blocks"""
- 
-    chunk_size = len(data) // processes #calculate chunk of data which should be sorted by this process
-    chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)] #split data to chunks
 
-    pool = multiprocessing.Pool(processes=processes) #stworzenie procesu
-    sorted_chunks = pool.map(sorted, chunks)
+    with multiprocessing.Pool(processes=processes) as pool:
+        sorted_chunks = pool.map(check_sort, data)
 
-    pool.close() #konczenie pracy multiProcesow
-    pool.join()
-
-    while len(sorted_chunks) > 1: #Merge sorted chunks
+    while len(sorted_chunks) > 1:
         merged_chunks = []
         for i in range(0, len(sorted_chunks), 2):
             if i + 1 < len(sorted_chunks):
